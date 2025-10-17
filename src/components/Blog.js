f180bd901771145ca849d6ed28e156a1adcfc4fe
mansour123456789 <bouchniba1997.mansour@gmail.com>
2025-10-17 02:38:41 +0200

    fixesss vercel5

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -18,7 +18,7 @@ const Blog = () => {
               <div className="blog-img">
                 
                   <a>
-                    <Image src="/public/static/img/scrum.JPG" alt="Scrum Certification" width={500} height={400} />
+                    <Image src="/static/img/scrum.JPG" alt="Scrum Certification" width={500} height={400} />
                   </a>
                
               </div>
